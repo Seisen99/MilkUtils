390acd2a9fad4120f2f76bfa2e7fe6466ac45179
Seisen99 <lpfrancisco99@gmail.com>
2025-10-27 06:34:08 +0000

    Fix @require URLs: point to master branch instead of feature branch

--- mwi-avatar-plus/mwi-avatar-plus.user.js
+++ mwi-avatar-plus/mwi-avatar-plus.user.js
@@ -12,26 +12,26 @@
 // @grant        none
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/utils/geometry.js?v=1
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/utils/colors.js?v=1
-// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/modern-settings-ui/mwi-avatar-plus/modules/core/constants.js?v=4
-// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/modern-settings-ui/mwi-avatar-plus/modules/core/settings.js?v=4
-// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/modern-settings-ui/mwi-avatar-plus/modules/ui/styles.js?v=7
-// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/modern-settings-ui/mwi-avatar-plus/modules/ui/toast.js?v=1
-// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/modern-settings-ui/mwi-avatar-plus/modules/ui/color-picker.js?v=2
-// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/modern-settings-ui/mwi-avatar-plus/modules/ui/settings-panel.js?v=10
-// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/modern-settings-ui/mwi-avatar-plus/modules/avatar/avatar-manager.js?v=2
+// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/core/constants.js?v=4
+// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/core/settings.js?v=4
+// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/ui/styles.js?v=7
+// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/ui/toast.js?v=1
+// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/ui/color-picker.js?v=2
+// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/ui/settings-panel.js?v=10
+// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/avatar/avatar-manager.js?v=2
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/avatar/avatar-observer.js?v=1
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/abilities_database.js?v=2
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/animations/utils/svg-paths.js?v=1
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/animations/animation-manager.js?v=1
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/animations/effects/miss-effect.js?v=1
-// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/modern-settings-ui/mwi-avatar-plus/modules/animations/effects/hit-effect.js?v=13
+// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/animations/effects/hit-effect.js?v=13
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/animations/effects/healing-particles.js?v=1
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/animations/effects/dot-effect.js?v=1
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/animations/projectiles/fireball.js?v=1
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/animations/projectiles/arrow.js?v=1
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/animations/projectiles/melee.js?v=1
-// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/modern-settings-ui/mwi-avatar-plus/modules/animations/effect-coordinator.js?v=6
-// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/modern-settings-ui/mwi-avatar-plus/modules/core/websocket.js?v=30
+// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/animations/effect-coordinator.js?v=6
+// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/master/mwi-avatar-plus/modules/core/websocket.js?v=30
 // ==/UserScript==
 
 (function() {
