4359c7bf853155ed479a193c52b87335bd089cc2
Seisen99 <lpfrancisco99@gmail.com>
2025-10-28 10:22:31 +0000

    Bump hit-effect.js to v14 for cache busting

--- mwi-avatar-plus/mwi-avatar-plus.user.js
+++ mwi-avatar-plus/mwi-avatar-plus.user.js
@@ -25,7 +25,7 @@
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/spritesheet-animations/mwi-avatar-plus/modules/animations/utils/svg-paths.js?v=1
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/spritesheet-animations/mwi-avatar-plus/modules/animations/animation-manager.js?v=1
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/spritesheet-animations/mwi-avatar-plus/modules/animations/effects/miss-effect.js?v=1
-// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/spritesheet-animations/mwi-avatar-plus/modules/animations/effects/hit-effect.js?v=13
+// @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/spritesheet-animations/mwi-avatar-plus/modules/animations/effects/hit-effect.js?v=14
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/spritesheet-animations/mwi-avatar-plus/modules/animations/effects/healing-particles.js?v=1
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/spritesheet-animations/mwi-avatar-plus/modules/animations/effects/dot-effect.js?v=1
 // @require      https://raw.githubusercontent.com/Seisen99/MilkUtils/feature/spritesheet-animations/mwi-avatar-plus/modules/animations/projectiles/fireball.js?v=1
